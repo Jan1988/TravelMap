3d88416cf59524f6a1524878e4d4965c9dc759e2
Jan1988 <jackycoke@gmx.de>
2020-04-01 21:55:23 +0200

    improve keys security

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ const path = require('path');
 //import Routs
 const apiRoutes = require("./routes/api-routes.js");
 const viewRoutes = require("./routes/view-routes.js");
+const mapRoutes = require("./routes/map-routes.js");
 
 
-var kommandozeile = process.argv;
-console.log(kommandozeile);
+// var kommandozeile = process.argv;
+// console.log(kommandozeile);
 
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
@@ -29,6 +30,7 @@ app.use(bodyParser.json());
 // Import routes
 app.use('/api', apiRoutes);
 app.use('/', viewRoutes);
+app.use('/map', mapRoutes);
 
 // middleware libraries
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,32 +44,3 @@ app.listen(port, function () {
 // app.use(express.static(__dirname + '/public'));
 // app.use(require('./middlewares/users'));
 
-
-
-// app.get('/', function(req, res) {
-//     var indexPath = 'C:/xampp/htdocs/TravelMap - Node.js/testin2.html';
-//     fs.exists(indexPath, function (exist) {
-//         if(!exist) {
-//           // if the file is not found, return 404
-//           res.statusCode = 404;
-//           res.end(`File ${indexPath} not found!`);
-//           return;
-//         }
-    
-//         // if is a directory search for index file matching the extention
-//         if (fs.statSync(indexPath).isDirectory()) indexPath += '/index' + ext;
-    
-//         // read file from file system
-//         fs.readFile(indexPath, function(err, data){
-//           if(err){
-//             res.statusCode = 500;
-//             res.end(`Error getting the file: ${err}.`);
-//           } else {
-//             // if the file is found, set Content-type and send data
-//             res.setHeader('Content-type', 'text/html' || 'text/plain' );
-//             res.end(data);
-//           }
-//         });
-//       });
-// });
-// Use Api routes in the App
--- routes/map-routes.js
+++ routes/map-routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+require('dotenv').config();
 
-router.get('/map', function (req, res) {
+
+router.get('/', function (req, res) {
     // console.log(req.params.view);
     // let viewFile = req.params.view;
-    res.sendFile(path.join(process.cwd(), 'views', 'map.html'));
+    let googleMapsKey = process.env.MAP_API_KEY;
+    console.log(googleMapsKey);
+    // res.sendFile(path.join(process.cwd(), 'views', 'map.html'));
 });
+
+module.exports = router;
